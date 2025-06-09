84b6894bdb572662e8a0548a05ea93e1ef1a0121
Juliana Crystal Holder <114365616+kiiing-crystal@users.noreply.github.com>
2025-06-09 13:52:32 +0200

    icons fix

--- crop-guard-ai/vite.config.js
+++ crop-guard-ai/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
